6060c415180fdb2a4a3b6b1af6112626b2c7c875
kunalhedaoodevops <kunalhedaoodevops@gmail.com>
2026-04-05 02:51:15 +0530

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -15,18 +15,16 @@ const Career = () => {
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Position In Company</h4>
-                <h5>Company Name</h5>
+                <h4>Devops-Server Engineer</h4>
+                <h5>Techspace Solutions</h5>
               </div>
-              <h3>20XX</h3>
+              <h3>2025</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
-            </p>
+  Worked as a DevOps-Server Engineer at Techspace Solutions, Pune, contributing to projects like Delmar and FarmCloud by leading end-to-end ETL development and managing data integration workflows with high data quality. Managed and maintained server infrastructure and PostgreSQL databases, ensuring performance, security, and reliability. Built and optimized CI/CD pipelines using GitLab, automated deployments, and handled containerization with Docker and Kubernetes across multiple environments. Configured Nginx web servers, implemented SSL using Certbot, and managed DNS infrastructure for secure operations. Performed database migration from OpenERP to Odoo 14 with minimal downtime. Monitored systems using AWS CloudWatch and New Relic, troubleshot issues, and collaborated with cross-functional teams to deliver scalable and high-availability solutions using AWS, Terraform, and Python scripting.
+</p>
           </div>
-          <div className="career-info-box">
+          {/* <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
                 <h4>Position In Company</h4>
@@ -39,7 +37,7 @@ const Career = () => {
               labore sit non ipsum temporibus quidem, deserunt eaque officiis
               mollitia ratione suscipit repellat.
             </p>
-          </div>
+          </div> */}
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
